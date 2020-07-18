dda6a00555e1abedeebd3652a5412c7f073b2452
ikikika <iki.jito@gmail.com>
2020-07-18 23:34:03 +0800

    create basic views

--- django1/django1/adoptions/views.py
+++ django1/django1/adoptions/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse('<h1>Home view</h1>')
+
+def pet_detail(request, pet_id):
+    return HttpResponse(f'<p>detail view with id {pet_id}</p>')
