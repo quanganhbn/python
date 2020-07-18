1b2ef539676e3f2b5b080c82c5383280cbf541eb
ikikika <iki.jito@gmail.com>
2020-07-18 23:43:21 +0800

    implement views and basic template

--- django1/django1/adoptions/views.py
+++ django1/django1/adoptions/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
+
+from .models import Pet
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Home view</h1>')
+    pets = Pet.objects.all()
+    return render(
+        request, 
+        'home.html', 
+        {'pets': pets}
+    )
 
 def pet_detail(request, pet_id):
-    return HttpResponse(f'<p>detail view with id {pet_id}</p>')
+    try:
+        pet = Pet.objects.get(id=pet_id)
+    except Pet.DoesNotExist:
+        raise Http404('pet not found')
+    return render(
+        request,
+        'pet_detail.html',
+        {'pet': pet}
+    )
