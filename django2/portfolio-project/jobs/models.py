9ecd8624f9bd3a90b9853f459a4c34d28f741a13
ikikika <iki.jito@gmail.com>
2020-07-24 22:08:35 +0800

    pull objects from db

--- django2/portfolio-project/jobs/models.py
+++ django2/portfolio-project/jobs/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Job(models.Model):
     # Image
-    image = models.ImageField(upload_to='iamges/')
+    image = models.ImageField(upload_to='images/')
     # Summary
     summary = models.CharField(max_length=200)
